565fd0c4afef6d1b45fef97154bd365af7c083ce
John Mwaura <johnmwaura08@gmail.com>
2020-05-30 22:53:22 -0500

    imported productconsumer from context api

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 // import Product from "./Product";
 import Title from "./Title";
 import {storeProducts} from '../../src/data';
+import{ProductConsumer} from '../context';
 
 export default class ProductList extends Component {
   state = {
@@ -15,7 +16,13 @@ export default class ProductList extends Component {
           <div className="container">
             <Title name="our" title="gadgets" />
             {/* //products row */}
-            <div className="row"></div>
+            <div className="row">
+                <ProductConsumer>
+                    {(value)=>{
+                        return <h1>{value}</h1> // whatever value in context will be returned here
+                    }}
+                </ProductConsumer>
+            </div>
           </div>
         </div>
 
