a332ee6b733d17b6c4521023752d6d344f55fa18
John Mwaura <johnmwaura08@gmail.com>
2020-06-01 23:05:12 -0500

    added images and styled image container

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
+import { FaCartArrowDown } from "react-icons/fa";
 
 export default class Product extends Component {
-    render() {
-        return (
-            <div>
-                <h3> Hello from product</h3>
-            </div>
-        )
-    }
+  render() {
+    const { id, title, img, price, inCart } = this.props.product;
+    return (
+      <ProductWrapper className={"col-9 mx-auto col-md-6 col-lg-3"}>
+        <div className="card">
+          <div
+            className="img-container p-5 "
+            onClick={() => console.log("you clicked on the image container")}
+          >
+            <Link to="/details">
+              <img src={img} alt="product iamge" className="card-img-top" />
+            </Link>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => {
+                console.log("added to cart");
+              }}
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  inCart
+                </p>
+              ) : (
+                <FaCartArrowDown />
+              )}
+            </button>
+          </div>
+        </div>
+      </ProductWrapper>
+    );
+  }
 }
+
+const ProductWrapper = styled.div``;
