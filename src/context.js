fdc9a659c0b5cc15c21bbfa9c939522334a2f4df
John Mwaura <johnmwaura08@gmail.com>
2020-06-01 22:28:18 -0500

    changed value to an object and added methods

--- src/context.js
+++ src/context.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import{ storeProducts, detailProduct} from './data'
 
 const ProductContext = React.createContext();
 //provider
 //consumer - uses information provided by provider
 
  class ProductProvider extends Component {
+     state={
+         products: storeProducts,
+         detailProduct: detailProduct
+
+     }
+     handleDetail = () => {
+            console.log( 'handleDetail')
+     }
+     addToCart = () => {
+         console.log('add to cart' )
+     }
     render() {
 
 
         return (
-            <ProductContext.Provider value="hello from context">
+            <ProductContext.Provider value={{ //value can be an object with methods and we can access it on productlist
+ ...this.state,
+ handleDetail: this.handleDetail,
+ addToCart: this.addToCart,
+
+            }}>
                 {this.props.children}
             </ProductContext.Provider>
         )
