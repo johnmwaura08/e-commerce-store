428677c011c2113697eb89868dda65eb0ceaf69d
John Mwaura <johnmwaura08@gmail.com>
2020-06-17 20:45:05 -0500

    added placeholder value function

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {ProductConsumer} from '../context';
+import {Link} from 'react-router-dom';
+import{ButtonContainer} from './Button'
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-                <h3> hello from details</h3>
-                
-            </div>
+            <ProductConsumer>
+                {(value)=>{
+                    const {id,company,img,info,price,title,inCart}= value.detailProduct;
+                    return(
+                        <div className="container py-5">
+                            <div className="row">
+                                <div className
+                            </div>
+                        </div>
+                    )
+                    
+                }}
+            </ProductConsumer>
+        
         )
     }
 }
