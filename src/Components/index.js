6d9abd43bdf845c5f166cafb7ee31875ddf7dec5
John Mwaura <johnmwaura08@gmail.com>
2020-09-17 17:42:28 -0500

    imported modal in index file

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,5 +3,6 @@ export { default as Default } from './Default';
 export { default as Details } from './Details';
 export { default as Navbar } from './Navbar';
 export { default as Product } from './Product';
+export { default as Modal } from './Modal';
 export { default as ProductList} from './ProductList';
 
