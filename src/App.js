9262128264ddd8f4f7d488758c4ca87ac47e88f5
John Mwaura <johnmwaura08@gmail.com>
2020-09-17 17:41:41 -0500

    imported modal file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Cart, Default, Details, Navbar, ProductList } from "./Components";
-
+import {
+  Cart,
+  Default,
+  Details,
+  Navbar,
+  ProductList,
+  Modal,
+} from "./Components";
 
 class App extends React.Component {
   render() {
@@ -11,15 +17,14 @@ class App extends React.Component {
       <React.Fragment>
         <Navbar />
         <Switch>
-          <Route exact path ="/" component = {ProductList}/>
-          <Route path ="/details" component = {Details}/>
-          <Route path ="/cart" component = {Cart}/>
-          <Route  component = {Default}/>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
 
+          <Route component={Default} />
         </Switch>
-        
-    
-        
+
+        <Modal />
       </React.Fragment>
     );
   }
