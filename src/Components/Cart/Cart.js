58ffbe543d59884b3a0cfe3806003318acd4d4f7
John Mwaura <johnmwaura08@gmail.com>
2020-09-17 19:46:25 -0500

    passed down props to cartlist component

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,6 +3,8 @@ import Title from '../Title';
 import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
 import {ProductConsumer} from '../../context';
+import Cartlist from './CartList'
+import CartList from './CartList';
 export default class Cart extends Component {
     render() {
         return (
@@ -15,7 +17,7 @@ export default class Cart extends Component {
                                <React.Fragment>
                                 <Title name ="your" title="cart" />
                                 <CartColumns />
-
+                                <CartList  value={value}/>
                                </React.Fragment>
                            )
                        } else {
