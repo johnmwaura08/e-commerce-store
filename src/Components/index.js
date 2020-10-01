1191aa2a421f4c609591217419701c59d4b3b636
John Mwaura <johnmwaura08@gmail.com>
2020-10-01 03:01:46 -0500

    added navbar and landing page exports

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,8 +1,9 @@
 export { default as Cart } from './Cart/Cart';
 export { default as Default } from './Default';
 export { default as Details } from './Details';
-export { default as Navbar } from './Navbar';
+export { default as Navbar } from './NavBar/Navbar';
 export { default as Product } from './Product';
 export { default as Modal } from './Modal';
 export { default as ProductList} from './ProductList';
+export {default as LandingPage} from './LandingPage/LandingPage';
 
