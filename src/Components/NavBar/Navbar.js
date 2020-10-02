d3d3552dace0dad73f938dbf7b36cbf0015caff7
John Mwaura <johnmwaura08@gmail.com>
2020-10-02 01:55:27 -0500

    added link for landing page and contact form

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,35 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
-import styled from "styled-components";
-
 import styles from "./NavBar.module.css";
 import wavylogo from "../../assets/img/wavylogo.svg";
 import cart from "../../assets/img/cart.svg";
 export default class Navbar extends Component {
   render() {
     return (
-   
       <div className={styles.container}>
         <div className={styles.logoContainer}>
-          <Link to="/">
-            <img
-        
-              src={wavylogo}
-              alt="logo"
-            />
+          <Link to="/landingpage">
+            <img src={wavylogo} alt="logo" />
           </Link>
 
           <h4 className={styles.logoz}>Wavy Kits</h4>
         </div>
         <nav>
           <ul className={styles.navLinks}>
-            <Link to="/products" style={{ textDecoration: 'none' }}>
-
-            <li className={styles.navLink} >Products</li>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <li className={styles.navLink}>Products</li>
             </Link>
-            <li className={styles.navLink}>Register</li>
+            <Link to="/contact" style={{ textDecoration: "none" }}>
             <li className={styles.navLink}>Contact </li>
+            </Link>
           </ul>
         </nav>
         <Link to="./cart">
@@ -41,4 +33,3 @@ export default class Navbar extends Component {
     );
   }
 }
-
