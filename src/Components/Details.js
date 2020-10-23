49eb7436c4b07c6e0bb717437fcfd52f0fe2c4ca
John Mwaura <johnmwaura08@gmail.com>
2020-10-22 23:56:39 -0500

    switched route name for productlist  to /products

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -41,7 +41,7 @@ export default class Details extends Component {
                                     <p className="text-muted lead">{info}</p>
                                     {/* buttons */}
                                     <div>
-                                        < Link to='/'>
+                                        < Link to='/products'>
                                             <ButtonContainer> back to products</ButtonContainer>
                                             <ButtonContainer
                                             cart 
