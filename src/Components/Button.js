840a9dc2d1c1e4200220b9bacc75800ed2c56e19
John Mwaura <johnmwaura08@gmail.com>
2020-10-30 20:43:03 -0500

    changed button color and background

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 const ButtonContainer = styled.button`
   text-transform: capitalize;
   font-size: 1.4rem;
-  /* background: transparent; */
-  border: 0.05 rem solid #611f69;
-  color: #611f69;
-  border-color:${props=> props.cart?"#F8E919":"#611f69"};
-  color:${props=> props.cart?"#F8E919":"#611f69"};
+  background: transparent;
+  border: 0.5 rem solid #611f69;
+  /* color: #611f69; */
+  color:#2E242C;
+  border-color:${(props) => (props.cart ? "#F8E919" : "#c36cbb")};
+  /* color:${(props) => (props.cart ? "black" : "#611f69")}; */
+  background-color:${(props) => (props.cart ? "#F8E919" : "#c36cbb")};
 
 
   
@@ -20,8 +22,9 @@ const ButtonContainer = styled.button`
       
       /* color: #00FF00; */
   /* color:#f469a9; */
-  color:#7c4183;
-  background: ${props=> props.cart?"#F8E919":"#611f69"};
+  /* color:#7c4183; */
+  color: black;
+  background: ${(props) => (props.cart ? "#F8E919" : "#c36cbb")};
 
   }
   &:focus{
