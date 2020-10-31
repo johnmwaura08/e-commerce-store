089191855e97b83ce8d36ee84da5874454bbfb71
John Mwaura <johnmwaura08@gmail.com>
2020-10-30 20:44:31 -0500

    centered checkout button and totals container

--- src/Components/Cart/CartTotals.js
+++ src/Components/Cart/CartTotals.js
@@ -7,7 +7,7 @@ export default function CartTotals({ value, history }) {
       <React.Fragment>
           <div className="container">
               <div className="row">
-                  <div className="col-10 mt-2 ml-sm-5 ml-auto col-sm-8 text-capitalize text-right">
+                  <div className="col-10 mt-2 ml-sm-5 ml-auto col-sm-8 text-capitalize text-center">
                     <Link to ="/products" >
                         <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={() => clearCart()}>
                             clear cart
