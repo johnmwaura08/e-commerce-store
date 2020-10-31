e89a6f7501b709d06e2b4bda9fceda7050b4c519
John Mwaura <johnmwaura08@gmail.com>
2020-10-30 20:47:38 -0500

    add info details on kits and add name to object

--- src/data.js
+++ src/data.js
@@ -6,166 +6,177 @@ https://www.iconfinder.com/Makoto_msk */
 export const storeProducts = [
   {
     id: 1,
-    title: "PARIS ST-GERMAIN HOME KIT",
+    title: "PSG (H) ",
+    name: "Paris St-Germain Home Kit",
     img: "kits/pasg.jpg",
-    price: 65.99,
+    price: 60,
     brand: "Nike",
-    info:"Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
+    info:
+      "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 2,
-    title: "LIVERPOOL FC HOME KIT",
+    title: "LIVERPOOL (H) ",
+    name: "Liverpool FC Home Kit",
     // img: "img/product-2.png",
-    img:"kits/images (1).jpg", 
-    
-    price: 89.99,
+    img: "kits/images (1).jpg",
+
+    price: 80,
     brand: "New Balance",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 3,
-    title: "FC BARCELONA HOME KIT",
+    title: "BARCELONA (H)",
+    name: "FC Barcelona Home Kit",
     img: "kits/images (3).jpg",
-    price: 64.99,
+    price: 60,
     brand: "Nike",
-    info:"The great return of the stripes on the classic Blaugrana jersey, but this year they are of exceptional importance for footballers, as well as fans.The motif of vertical stripes along with the yellow collar is a reference to the jersey from the 2010/11 season, when the team from Barcelona won the Spanish championship and the Champions League.On the collar of the jersey there is an insert in the colors of Senyery i.e. the famous Catalan flag. The club from the capital of the region has repeatedly emphasized its origin and also this year decided to put such an emphasis on its most important kit.The patch with the serial code at the bottom and the original labels are proof of authenticity - we only import jerseys from the official source",
+    info:
+      "The great return of the stripes on the classic Blaugrana jersey, but this year they are of exceptional importance for footballers, as well as fans.The motif of vertical stripes along with the yellow collar is a reference to the jersey from the 2010/11 season, when the team from Barcelona won the Spanish championship and the Champions League.On the collar of the jersey there is an insert in the colors of Senyery i.e. the famous Catalan flag. The club from the capital of the region has repeatedly emphasized its origin and also this year decided to put such an emphasis on its most important kit.The patch with the serial code at the bottom and the original labels are proof of authenticity - we only import jerseys from the official source",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 4,
-    title: "MANCHESTER CITY HOME KIT",
+    title: "MAN CITY (H)",
+    name: "Manchester City Home Kit",
     img: "kits/images (2).jpg",
-    price: 99.99,
+    price: 90,
     brand: "Nike",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 5,
-    title: "ATLETICO MADRID AWAY KIT",
+    title: "ATLETICO (A)",
+    name: " Atletico Madrid Away Kit",
     img: "kits/images (5).jpg",
-    price: 78.99,
+    price: 70,
     brand: "Nike",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 6,
-    title: "MANCHESTER UNITED HOME KIT",
+    title: "MAN UTD (H)",
+    name: " Manchester United Home Kit",
     img: "kits/images (4).jpg",
-    price: 119.99,
+    price: 110,
     brand: "Adidas",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
-  
+
   {
     id: 7,
-    title: "BORUSSIA DORTMUND HOME KIT",
+    title: "BVB (H)",
+    name: " Borussia Dortmund Home Kit",
     img: "kits/images (8).jpg",
-    price: 69.99,
+    price: 60,
     brand: "Puma",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 8,
-    title: "CHELSEA FC HOME KIT",
+    title: "CHELSEA (H)",
+    name: "Chelsea FC Home Kit",
     img: "kits/images (6).jpg",
-    price: 119.99,
+    price: 110,
     brand: "Nike",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
     inCart: false,
     count: 0,
-    total: 0
-  }
-  ,
+    total: 0,
+  },
   {
     id: 9,
-    title: "REAL MADRID HOME KIT",
+    title: "REAL MADRID (H)",
+    name: "Real Madrid Home Kit",
     img: "kits/images.jpg",
-    price: 99.99,
+    price: 90,
     brand: "Adidas",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
-  
+
   {
     id: 10,
-    title: "FC BAYERN MUNICH AWAY KIT",
+    title: "BAYERN (A)",
+    name: "FC Bayern Munich Away Kit",
     img: "kits/images (7).jpg",
-    price: 65.99,
+    price: 60,
     brand: "Adidas",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
-  
+
   {
     id: 11,
-    title: "CHRISTIANO RONALDO ICONIC JERSEY",
+    title: "CR7",
+    name: "CHRISTIANO RONALDO ICONIC JERSEY",
     img: "kits/ronaldo.jpg",
-    price: 159.99,
+    price: 150,
     brand: "Adidas",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
     inCart: false,
     count: 0,
-    total: 0
-  }
-  ,
+    total: 0,
+  },
   {
     id: 12,
-    title: "TOTTENHAM HOTSPUR HOME KIT",
+    title: "SPURS (H)",
+    name: "Tottenham Hotspurs Home Kit",
     img: "kits/images (9).jpg",
-    price: 77.99,
+    price: 77,
     brand: "Nike",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
     inCart: false,
     count: 0,
-    total: 0
-  }
-
-  
-  
+    total: 0,
+  },
 ];
 //placeholder
 export const detailProduct = {
   id: 1,
-    title: "PARIS ST-GERMAIN HOME KIT",
-    img: "kits/pasg.jpg",
-    price: 65.99,
-    brand: "Nike",
-    info:"Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
-    inCart: false,
-    count: 0,
-    total: 0
+  title: "PSG (H) ",
+  name: "Paris St-Germain Home Kit",
+  img: "kits/pasg.jpg",
+  price: 60,
+  brand: "Nike",
+  info:
+    "Jersey fits true to size ,Material is 100% Polyester,Nike Breathe fabrics move sweat from your skin and feature highly breathable construction to help you stay dry and comfortable,Dri-FIT technology that  wicks away moisture,Woven Authentic Nike jock tag on left hem,Embroidered Nike ® logo on right chest,Sewn on embroidered team crest on left chest.and is Machine washable",
+  inCart: false,
+  count: 0,
+  total: 0,
 };
